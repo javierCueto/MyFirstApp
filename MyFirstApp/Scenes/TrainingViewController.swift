03a1c1f9c4fe0bf0cfcab9ac19a24f686490cc79
Javier Cueto <39851501+javierCueto@users.noreply.github.com>
2023-12-12 08:12:16 -0700

    Course iOS I - Part 1

---
+++ MyFirstApp/Scenes/TrainingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrainingViewController.swift
+//  MyFirstApp
+//
+//  Created by Javier Cueto on 11/12/23.
+//
+
+import UIKit
+
+class TrainingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -15,5 +15,14 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func trainingButton(_ sender: Any) {
+        print("clicked")
+        let viewController: UIViewController = TrainingViewController()
+        
+        //viewController.modalPresentationStyle = .fullScreen
+        //UIStoryboard(name: "TrainingStoryboard", bundle: nil)
+                   //.instantiateViewController(withIdentifier: "Training") as UIViewController
+        present(viewController, animated: true)
+    }
 }
 
